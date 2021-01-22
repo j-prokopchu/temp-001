572fa69805ca9ff17d4e5abd1a26ddf8c6b19476
j-prokopchu <75361087+j-prokopchu@users.noreply.github.com>
2021-01-22 15:28:38 +0300

    2

---
+++ script.js
@@ -0,0 +1,25 @@
+let stpbl1 = document.getElementById("stepbl1");
+let stpbrd1 = document.getElementById("stepbrdr1")
+let stpbl2 = document.getElementById("stepbl2");
+let stpbrd2 = document.getElementById("stepbrdr2")
+let stpbl3 = document.getElementById("stepbl3");
+let stpbrd3 = document.getElementById("stepbrdr3")
+
+stpbl1.onmouseover = function(){
+    stpbrd1.style.borderBottom = "3px solid while";
+}
+stpbl1.onmouseout = function(){
+    stpbrd1.style.borderBottom = "3px solid black";
+}
+stpbl2.onmouseover = function(){
+    stpbrd2.style.borderBottom = "3px solid while";
+}
+stpbl2.onmouseout = function(){
+    stpbrd2.style.borderBottom = "3px solid black";
+}
+stpbl3.onmouseover = function(){
+    stpbrd3.style.borderBottom = "3px solid while";
+}
+stpbl3.onmouseout = function(){
+    stpbrd3.style.borderBottom = "3px solid black";
+}
